7aa71925b87db0dad034936e3b8e4f3cf8ae2328
ducanh1910 <anhnguyenduc1910@gmail.com>
2023-10-20 21:57:41 +0700

    Create index.js

---
+++ src/components/Films/Pages/News/index.js
@@ -0,0 +1,15 @@
+import styles from './News.module.scss';
+import classNames from 'classnames/bind';
+
+// eslint-disable-next-line no-unused-vars
+const cx = classNames.bind(styles);
+function News() {
+    return (
+        <div>
+            <h1>Hãy đón chờ các bộ phim nóng hổi tại đây nhé!!!</h1>
+            
+        </div>
+    );
+}
+
+export default News;
