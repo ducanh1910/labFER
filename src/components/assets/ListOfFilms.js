ce405881d56ec8c448361ce40b805192aaccb9b2
ducanh1910 <anhnguyenduc1910@gmail.com>
2023-10-20 21:57:09 +0700

    Create ListOfFilms.js

---
+++ src/components/assets/ListOfFilms.js
@@ -0,0 +1,60 @@
+const ListOfFilms = [
+    {
+      id: 1,
+      Image: require("./Loki.jpg"), 
+      Title: "Loki ",
+      Year: "1977",
+      Nation: "USA",
+    },
+    {
+        id: 2,
+        Image: require("./An-Mang-O-Venice.jpg"), 
+        Title: "An mang o Venice ",
+        Year: "1977",
+        Nation: "USA",
+    },
+    {
+        id: 3,
+        Image: require("./Chucky-3.jpg"), 
+        Title: "Chucky 3 ",
+        Year: "1977",
+        Nation: "USA",
+    },
+    {
+        id: 4,
+        Image: require("./Elemental.jpg"), 
+        Title: "Elemental ",
+        Year: "1977",
+        Nation: "USA",
+    },
+    {
+        id: 5,
+        Image: require("./Gen-V.jpg"), 
+        Title: "Gen V ",
+        Year: "1977",
+        Nation: "USA",
+    },
+    {
+        id: 6,
+        Image: require("./meg-2.jpg"), 
+        Title: "Megalondon 2 ",
+        Year: "1977",
+        Nation: "USA",
+    },
+    {
+        id: 7,
+        Image: require("./One-Piece.jpg"), 
+        Title: "One piece ",
+        Year: "1977",
+        Nation: "USA",
+    },
+    {
+        id: 8,
+        Image: require("./Phim-Blue-Beetle.jpg"), 
+        Title: "Blue beetle ",
+        Year: "1977",
+        Nation: "USA",
+    },
+  ];
+  
+  export default ListOfFilms;
